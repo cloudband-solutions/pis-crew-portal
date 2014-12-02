bf85198d17c52f0a99dec5e467161314f54eafb0
kathleenlogmao <kathleenlogmao@gmail.com>
2014-12-02 16:26:10 +0800

    hi

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,22 @@ class PagesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-  end
-
-  def profile
-    result = Curl.get("#{Settings.pis_server_url}/crews/#{current_user.crew_token}?access_token=#{Settings.access_token}")
+     result = Curl.get("#{Settings.pis_server_url_api}/crews/#{current_user.crew_token}?access_token=#{Settings.access_token}")
 
     if result.response_code == 200
       @crew = JSON.parse(result.body_str)
+      puts @crew
+    else
+      raise "Error"
+    end
+  end
+
+  def profile
+    result = Curl.get("#{Settings.pis_server_url_api}/crews/#{current_user.crew_token}?access_token=#{Settings.access_token}")
+
+    if result.response_code == 200
+      @crew = JSON.parse(result.body_str)
+      puts @crew
     else
       raise "Error"
     end
